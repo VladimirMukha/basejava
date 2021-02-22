e645fef643aa6e81a142cb9298862290cb70a43b
Nirvanas <nirvanas1994@yahoo.com>
2021-02-22 20:11:35 +0300

    исправление 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private int element;
+    private int size;
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        //  Arrays.fill(storage,0,element,null);
-        element = 0;
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
         if (r.uuid != null) {
-            storage[element] = r;
-            element++;
+            storage[size] = r;
+            size++;
         }
     }
 
@@ -31,8 +31,8 @@ public class ArrayStorage {
     void delete(String uuid) {
         for (int i = 0; i < size(); i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i].uuid = storage[i + 1].uuid;
-                element--;
+                storage[i] = storage[i + 1];
+                size--;
             }
         }
     }
@@ -41,10 +41,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, element);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return element;
+        return size;
     }
 }
