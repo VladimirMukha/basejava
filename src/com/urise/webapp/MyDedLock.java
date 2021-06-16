e2f2ec32c13c009f341461e7da5e4f858404d76b
Nirvanas <nirvanas1994@yahoo.com>
2021-06-16 22:36:23 +0300

    homework 11 lesson fixes.

--- src/com/urise/webapp/MyDedLock.java
+++ src/com/urise/webapp/MyDedLock.java
@@ -5,24 +5,23 @@ public class MyDedLock {
     public static void main(String[] args) {
         String lockOne = "lock2";
         String lokTwo = "lock1";
-        new Thread(()->detLock(lockOne,lokTwo)).start();
-       new Thread(()->detLock(lokTwo,lockOne)).start();
+        new Thread(() -> detLock(lockOne, lokTwo)).start();
+        new Thread(() -> detLock(lokTwo, lockOne)).start();
     }
 
-    public static void detLock(String lockOne,String lockTwo ) {
+    public static void detLock(String lockOne, String lockTwo) {
         synchronized (lockOne) {
-            toWait();
-            synchronized (lockTwo) {
+            System.out.println(Thread.currentThread().getName() + " поток захватил " + lockOne
+                    + " Ожидает объект " + lockTwo);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }
-    }
-
-    public  static  void toWait() {
-        System.out.println(Thread.currentThread().getName());
-        try {
-           Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            synchronized (lockTwo) {
+                System.out.println(Thread.currentThread().getName() + " захватил " + lockTwo
+                        + " Ожидает объект " + lockOne);
+            }
         }
     }
 }
