35ce65cbb0f60bece203731f28264a05367a21fc
Nirvanas <nirvanas1994@yahoo.com>
2021-06-24 20:51:17 +0300

    homework 12 implement two method.

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,9 +1,6 @@
 package com.urise.webapp;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
@@ -41,23 +37,6 @@ public class MainConcurrency {
         System.out.println();
         System.out.println(mainConcurrency.counter);
         System.out.println(ATOMIC_INTEGER);
-        List<Integer> listInt = Arrays.asList(3, 2, 4, 2, 1);
-        int[] integerNumber = {1, 2, 3, 3, 2, 3};
-        MainConcurrency concurrency = new MainConcurrency();
-        System.out.println(concurrency.oddOrEven(listInt));
-        System.out.println(concurrency.minValue(integerNumber));
-    }
-
-    private int minValue(int[] values) {
-        List<Integer> result = Arrays.stream(values).distinct().boxed().sorted().collect(Collectors.toList());
-        return Integer.valueOf(result.stream().map(Objects::toString).collect(Collectors.joining()));
-    }
-
-    private List<Integer> oddOrEven(List<Integer> integers) {
-        return integers.stream().filter(integers.stream()
-                .mapToInt(Integer::intValue)
-                .sum() % 2 != 0 ? n -> n % 2 == 0 : n -> n % 2 == 1)
-                .collect(Collectors.toList());
     }
 
     private void inc() {
---
+++ src/com/urise/webapp/MainStreams.java
@@ -0,0 +1,29 @@
+package com.urise.webapp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStreams {
+    public static void main(String[] args) {
+        List<Integer> listInt = Arrays.asList(3, 2, 4, 2, 1, 1);
+        int[] integerNumber = {1, 2, 3, 3, 2, 3};
+        MainStreams mainStreams = new MainStreams();
+        System.out.println(mainStreams.oddOrEven(listInt));
+        System.out.println(mainStreams.minValue(integerNumber));
+
+    }
+
+    private int minValue(int[] values) {
+        return Arrays.stream(values).distinct().sorted().reduce((left, right) -> {
+                    String str = left + "" + right;
+                    return Integer.parseInt(str);
+                }
+        ).getAsInt();
+    }
+
+    private List<Integer> oddOrEven(List<Integer> integers) {
+        int oddOrEven = integers.stream().mapToInt(Integer::intValue).sum();
+        return integers.stream().filter(oddOrEven % 2 != 0 ? n -> n % 2 == 0 : n -> n % 2 == 1).collect(Collectors.toList());
+    }
+}
