14e34799e60e0a51b7b9c274036500595d936c42
Nirvanas <nirvanas1994@yahoo.com>
2021-06-25 23:49:37 +0300

    homework 12 implement two method.

--- src/com/urise/webapp/MainStreams.java
+++ src/com/urise/webapp/MainStreams.java
@@ -6,20 +6,26 @@ import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        List<Integer> listInt = Arrays.asList(3, 2, 4, 2, 1, 1);
+        List<Integer> listInt = Arrays.asList(3, 2, 4);
         int[] integerNumber = {1, 2, 2, 1, 4, 6};
-        MainStreams mainStreams = new MainStreams();
-        System.out.println(mainStreams.oddOrEven(listInt));
-        System.out.println(mainStreams.minValue(integerNumber));
+        System.out.println(oddOrEven(listInt));
+        System.out.println(minValue(integerNumber));
     }
 
-    private int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce((left, right) -> left * 10 + right
-        ).getAsInt();
+    private static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, ((left, right) -> left * 10 + right));
     }
 
-    private List<Integer> oddOrEven(List<Integer> integers) {
-        int oddOrEven = integers.stream().mapToInt(Integer::intValue).sum();
-        return integers.stream().filter(oddOrEven % 2 != 0 ? n -> n % 2 == 0 : n -> n % 2 == 1).collect(Collectors.toList());
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
+        return integers.stream().filter(s -> {
+            if (sum % 2 == 1) {
+                return s % 2 == 0;
+            } else
+                return s % 2 == 1;
+        }).collect(Collectors.toList());
     }
 }
