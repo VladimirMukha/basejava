ad99a0774b0071d0c868d2ca79e44b8f08cc9125
Nirvanas <nirvanas1994@yahoo.com>
2021-06-30 23:22:11 +0300

    homework 12 implement two method.

--- src/com/urise/webapp/MainStreams.java
+++ src/com/urise/webapp/MainStreams.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        List<Integer> listInt = Arrays.asList(3, 2, 4);
+        List<Integer> listInt = Arrays.asList(3, 2, 4,1,1);
         int[] integerNumber = {1, 2, 2, 1, 4, 6};
         System.out.println(oddOrEven(listInt));
         System.out.println(minValue(integerNumber));
@@ -21,11 +21,8 @@ public class MainStreams {
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         int sum = integers.stream().mapToInt(Integer::intValue).sum();
-        return integers.stream().filter(s -> {
-            if (sum % 2 == 1) {
-                return s % 2 == 0;
-            } else
-                return s % 2 == 1;
-        }).collect(Collectors.toList());
+        return integers.stream()
+                .filter(streamNumber -> streamNumber % 2 != sum % 2)
+                .collect(Collectors.toList());
     }
 }
